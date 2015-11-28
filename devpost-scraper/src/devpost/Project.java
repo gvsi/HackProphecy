f6cffd679ab1bf366341e04f6ecf32ca710e0c09
veniversum <hello@veniversum.me>
2015-11-28 19:11:28 +0000

    Allows spliting of scraping jobs across multiple instances

--- devpost-scraper/src/devpost/Project.java
+++ devpost-scraper/src/devpost/Project.java
@@ -5,7 +5,10 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class Project {
@@ -46,32 +49,38 @@ public class Project {
     }
 
     public void getProject(String url) throws IOException {
-        Document projectPage = Jsoup.connect(url).timeout(10000).get();
-        name = projectPage.select("#app-title").first().text();
         try {
-            blurp = projectPage.select("#app-title ~ p.large").first().text();
-        } catch (Exception ignored) {
-        }
-        likes = projectPage.select(".software-likes .side-count").isEmpty() ? 0 : Integer.parseInt(projectPage.select(".software-likes .side-count").first().text());
-        try {
-            description = projectPage.select("div#app-details-left").first().text();
-        } catch (Exception ignored) {
-        }
-        technologies.addAll(projectPage.select(".cp-tag").stream().map(Element::text).collect(Collectors.toList()));
-        try {
-            hackathon = projectPage.select(".software-list-content > p").text();
-        } catch (Exception ignored) {
-        }
-        winner = !projectPage.select(".winner").isEmpty();
-        for (Element e : projectPage.select(".software-team-member :not(figure) > .user-profile-link")) {
-            String link = e.attr("href");
-            String[] tmp = link.split("/");
-            String username = tmp[tmp.length - 1];
-            if (UserSet.getUserSet().containsKey(username))
-                authors.add(UserSet.getUserSet().get(username));
-            else {
-                authors.add(new User(link));
+            Document projectPage = Jsoup.connect(url).timeout(10000).get();
+            name = projectPage.select("#app-title").first().text();
+            try {
+                blurp = projectPage.select("#app-title ~ p.large").first().text();
+            } catch (Exception ignored) {
             }
+            likes = projectPage.select(".software-likes .side-count").isEmpty() ? 0 : Integer.parseInt(projectPage.select(".software-likes .side-count").first().text());
+            try {
+                description = projectPage.select("div#app-details-left").first().text();
+            } catch (Exception ignored) {
+            }
+            technologies.addAll(projectPage.select(".cp-tag").stream().map(Element::text).collect(Collectors.toList()));
+            try {
+                hackathon = projectPage.select(".software-list-content > p").text();
+            } catch (Exception ignored) {
+            }
+            winner = !projectPage.select(".winner").isEmpty();
+            for (Element e : projectPage.select(".software-team-member :not(figure) > .user-profile-link")) {
+                String link = e.attr("href");
+                String[] tmp = link.split("/");
+                String username = tmp[tmp.length - 1];
+                if (UserSet.getUserSet().containsKey(username))
+                    authors.add(UserSet.getUserSet().get(username));
+                else {
+                    authors.add(new User(link));
+                }
+            }
+        } catch (SocketTimeoutException e) {
+            Logger.getGlobal().log(Level.SEVERE, "Error fetching project " + url);
+            Logger.getGlobal().log(Level.WARNING, e.getMessage());
+            getProject(url); //Retry
         }
     }
 }
