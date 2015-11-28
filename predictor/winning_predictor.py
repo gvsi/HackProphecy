20a99690251a7ac5621621560b8fab1fafa2e4ad
Giovanni Alcantara <me@giovannialcantara.com>
2015-11-28 19:13:00 +0000

    Implement win vector

--- predictor/winning_predictor.py
+++ predictor/winning_predictor.py
@@ -5,4 +5,8 @@ with open('data.json') as data_file:
    data = json.load(data_file)
 
 #pprint([x for x in data if x['winner'] == True])
-print(len(data))
+#print(len(data))
+
+# Win vector
+win_vector = ["1" if x['winner'] else "0" for x in data]
+print(win_vector)
