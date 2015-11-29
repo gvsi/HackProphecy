ea334f5037ceb5bf9cf55df3910c4aa72b1fd346
veniversum <hello@veniversum.me>
2015-11-29 05:56:01 +0000

    Added more url patterns for web

--- www/hello.py
+++ www/hello.py
@@ -1,16 +1,20 @@
+import os
 from flask import Flask
 from flask import render_template
+from flask import jsonify
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def home():
     return render_template('index.html')
-@app.route('/<name>')
-def predict():
-    return render_template('predict.html')
+@app.route('/predict/<name>')
+def predict(name):
+    return jsonify(win=True,
+                  message='test')
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('page_not_found.html'), 404
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run()
+    
